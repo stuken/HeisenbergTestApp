fd5d8f603bcf4d8588a131318ccaa5b4871003b7
skenny <skenny@ws18.waratek.lan>
2014-10-06 11:59:08 +0100

    Added address table and sample data.

--- HeisenbergTestApp/WebContent/conf/setupdb.sql
+++ HeisenbergTestApp/WebContent/conf/setupdb.sql
@@ -18,6 +18,14 @@ CREATE TABLE users (
     cvv number
 );
 
+CREATE TABLE address (
+	id number,
+	address_1 varchar(500),
+	address_2 varchar(500),
+	address_3 varchar(500)
+)
+
+
 INSERT INTO users (id, name, surname) VALUES (1, 'luther', 'blisset');
 INSERT INTO users (id, name, surname) VALUES (2, 'fluffy', 'bunny');
 INSERT INTO users (id, name, surname) VALUES (3, 'wu', 'ming');
@@ -118,3 +126,104 @@ INSERT INTO users (id, name, surname, dob, credit_card, cvv) VALUES (97, 'Evan',
 INSERT INTO users (id, name, surname, dob, credit_card, cvv) VALUES (98, 'Alfred', 'Gonzalez', '06-Jul-1979', '4929 4310 6904 8848', 998);
 INSERT INTO users (id, name, surname, dob, credit_card, cvv) VALUES (99, 'Emilio', 'Maxwell', '25-Mar-1978', '3797 2892 3668 999', 788);
 INSERT INTO users (id, name, surname, dob, credit_card, cvv) VALUES (100, 'Rose', 'Terry', '13-Apr-1955', '5239 2173 5756 5789', 183);
+
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (1, '2128 Vestibulum,  St.', 'Dubuisson', 'San Marino');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (2, 'P.O. Box 893,  4620 Et,  Rd.', 'Louisville', 'Germany');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (3, 'P.O. Box 659,  868 Vel,  Street', 'Saint Andr', 'Botswana');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (4, '956-8957 Proin St.', 'Landelies', 'Paraguay');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (5, '368-4616 Vitae St.', 'Blue Mountains', 'Moldova');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (6, '979-8271 Nisi Street', 'L''Hospitalet de Llobregat', 'Macedonia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (7, 'Ap #923-3442 Sapien St.', 'Castello Tesino', 'Egypt');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (8, 'Ap #521-2326 Hendrerit Avenue', 'Betim', 'Guam');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (9, 'P.O. Box 252,  7309 Enim Av.', 'Shahjahanpur', 'Malaysia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (10, '2763 Non,  St.', 'Rotterdam', 'Lesotho');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (11, '1665 Ac Ave', 'Dubbo', 'Australia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (12, 'Ap #776-8758 Pede. Av.', 'Segni', 'Anguilla');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (13, '5188 Orci,  Avenue', 'Vihari', 'Germany');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (14, 'P.O. Box 146,  4268 Neque. Avenue', 'Castelluccio Inferiore', 'United States');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (15, 'Ap #567-9659 Blandit Av.', 'Langholm', 'Palestine,  State of');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (16, '9137 Purus,  Road', 'Ede', 'Afghanistan');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (17, '197-9767 Et Street', 'Grave', 'Mayotte');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (18, '3585 Imperdiet Ave', 'Köflach', 'Sint Maarten');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (19, '767-1258 Suspendisse Street', 'Borsbeek', 'Vanuatu');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (20, '543-2136 Risus. Ave', 'Ramagundam', 'Saint Helena,  Ascension and Tristan da Cunha');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (21, 'P.O. Box 865,  6049 Sed St.', 'Oostmalle', 'Seychelles');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (22, '853-9335 Pellentesque St.', 'Tay', 'Cook Islands');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (23, '486-6116 Enim. Ave', 'Lieferinge', 'Christmas Island');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (24, 'P.O. Box 107,  2658 Mattis Ave', 'Jabalpur', 'Aruba');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (25, '486-8514 Ut St.', 'Bergerac', 'Marshall Islands');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (26, '3490 Aliquet Rd.', 'Imst', 'Dominican Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (27, '263-1658 Vel,  Avenue', 'Chalon-sur-Saône', 'Sudan');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (28, '929-3045 Arcu St.', 'Bhilwara', 'Dominican Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (29, 'Ap #478-2303 Sodales Avenue', 'Werbomont', 'Trinidad and Tobago');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (30, '952-448 Ac Av.', 'Villers-Perwin', 'Uruguay');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (31, 'Ap #226-3945 Tempor Rd.', 'Gondiya', 'Timor-Leste');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (32, '936-2560 Eu Road', 'Newcastle-upon-Tyne', 'Macedonia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (33, 'Ap #520-361 Commodo St.', 'Pittsburgh', 'Liechtenstein');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (34, '519 Dictum Avenue', 'Alessandria', 'Djibouti');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (35, '716-3087 Arcu St.', 'Las Vegas', 'Belgium');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (36, 'Ap #590-2532 Nulla. Road', 'Great Falls', 'Yemen');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (37, 'P.O. Box 482,  6905 Aenean Av.', 'Cobourg', 'Mali');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (38, '7692 Ligula Avenue', 'Sangli', 'Somalia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (39, '842-8215 At,  Rd.', 'Halanzy', 'Lebanon');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (40, 'Ap #457-7394 Nam Ave', 'Evesham', 'Honduras');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (41, 'Ap #851-8124 Sem Street', 'Henstedt-Ulzburg', 'Macao');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (42, '4348 Quis,  Ave', 'Dorval', 'Nauru');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (43, '405 Risus. Rd.', 'Wimbledon', 'Timor-Leste');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (44, 'P.O. Box 817,  5857 Bibendum Rd.', 'Ottignies-Louvain-la-Neuve', 'Spain');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (45, 'P.O. Box 314,  5114 Quisque Street', 'Dreieich', 'Tanzania');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (46, '369-1371 Luctus Road', 'Berlin', 'Czech Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (47, 'P.O. Box 109,  7337 Donec Avenue', 'Vanderhoof', 'Belize');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (48, 'P.O. Box 715,  1305 Blandit Rd.', 'Lives-sur-Meuse', 'Barbados');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (49, '725-7910 Cubilia Avenue', 'Hamburg', 'Japan');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (50, '787-6105 Nec Rd.', 'Sorradile', 'French Polynesia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (51, 'Ap #258-715 Integer Avenue', 'Calmar', 'Reunion');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (52, '3429 Massa. Avenue', 'Bhind', 'Qatar');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (53, 'Ap #244-4671 Mattis. Rd.', 'Cessnock', 'Venezuela');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (54, '738-9669 Aenean Avenue', 'Tiverton', 'Eritrea');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (55, 'Ap #610-1393 Feugiat Ave', 'Mespelare', 'Dominica');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (56, '682-1526 Fames Rd.', 'Walsall', 'Eritrea');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (57, 'P.O. Box 247,  8568 Eu St.', 'Lampertheim', 'Hungary');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (58, '915-1934 At,  Road', 'Viano', 'Palau');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (59, 'Ap #970-3610 Auctor. St.', 'Grand-Rosire-Hottomont', 'United Arab Emirates');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (60, '809-6401 Et Ave', 'Sint-Andries', 'Croatia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (61, 'Ap #973-6776 Ullamcorper Avenue', 'Abbateggio', 'Mongolia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (62, '600-9398 Egestas. Ave', 'Couillet', 'Belgium');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (63, 'P.O. Box 864,  6267 At,  Avenue', 'Warwick', 'Haiti');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (64, '315-8322 Vivamus Rd.', 'Melsele', 'Monaco');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (65, 'Ap #532-4751 Vel Street', 'Anderlues', 'Dominican Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (66, 'Ap #418-2930 Massa St.', 'Houdemont', 'Hungary');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (67, '7764 At Av.', 'Idaho Falls', 'Slovenia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (68, 'Ap #575-8083 Ullamcorper,  Rd.', 'Vilvoorde', 'French Southern Territories');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (69, 'Ap #948-4793 Sodales St.', 'Sambreville', 'Saudi Arabia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (70, '160-6809 In Avenue', 'Alessandria', 'Maldives');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (71, '197-4211 Suscipit,  St.', 'Castel Colonna', 'Czech Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (72, 'P.O. Box 394,  5503 Nec,  Av.', 'Stockton-on-Tees', 'Pitcairn Islands');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (73, '5542 Mauris,  Road', 'Wilmington', 'Turkey');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (74, 'Ap #761-9847 Dapibus Avenue', 'Wilmont', 'New Caledonia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (75, 'Ap #760-7143 Lectus. Ave', 'Fontanafredda', 'Comoros');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (76, '820-1645 Consequat Av.', 'Brusson', 'Turkmenistan');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (77, '1231 Eros St.', 'Rattray', 'Cape Verde');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (78, '8065 A,  St.', 'Houtvenne', 'Guinea-Bissau');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (79, 'Ap #277-3293 Nisi St.', 'Caldarola', 'Korea,  South');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (80, 'P.O. Box 277,  618 Cursus St.', 'Sint-Andries', 'Malta');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (81, '279-6249 Posuere,  Street', 'Bamberg', 'Heard Island and Mcdonald Islands');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (82, 'P.O. Box 864,  2956 Malesuada St.', 'Chatillon', 'Myanmar');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (83, '4838 Vehicula. Rd.', 'Wallasey', 'Central African Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (84, '5737 Consectetuer Rd.', 'Molenbeersel', 'Bolivia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (85, '836-8517 Pede. Ave', 'Feltre', 'Czech Republic');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (86, 'P.O. Box 953,  3403 Aptent Street', 'Chicago', 'Liechtenstein');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (87, 'P.O. Box 317,  7845 Turpis. Street', 'Warspite', 'Pakistan');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (88, 'Ap #356-734 Cras Ave', 'Juneau', 'Isle of Man');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (89, 'P.O. Box 943,  7174 Volutpat Road', 'L''Hospitalet de Llobregat', 'Albania');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (90, '867-2210 Eu Rd.', 'Ashburton', 'Myanmar');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (91, 'P.O. Box 143,  9778 Nunc Avenue', 'Castelbaldo', 'Anguilla');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (92, '472-2801 Nonummy Street', 'Pitt Meadows', 'Botswana');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (93, 'Ap #438-3935 Integer Road', 'Tullibody', 'Liechtenstein');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (94, 'P.O. Box 718,  8902 Tellus. St.', 'Canning', 'Liechtenstein');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (95, '4126 Cubilia Road', 'Pisa', 'Iraq');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (96, '608-1826 Arcu. St.', 'Kearny', 'Malaysia');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (97, 'P.O. Box 169,  2049 Eu Avenue', 'Duncan', 'Burundi');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (98, '833-9890 Curabitur Rd.', 'Bierce', 'Cocos (Keeling) Islands');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (99, 'P.O. Box 135,  833 Id,  St.', 'Beaumaris', 'Syria');
+INSERT INTO address (id, address_1, address_2, address_3) VALUES (100, '881-6186 Pharetra. Ave', 'La Baie', 'United Arab Emirates');
